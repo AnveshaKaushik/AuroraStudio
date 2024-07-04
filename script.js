dfaa11497ecce11865763d158465168088f3501c
Anvesha Kaushik <anvesha.tyagi261@gmail.com>
2024-07-04 15:34:15 +0530

    Added the fixed image effect

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const scroll = new LocomotiveScroll({
+/*const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
+});*/
+
+var fixed = document.querySelector("#fixed-img")
+var elemContainer = document.querySelector("#elem-container")
+
+elemContainer.addEventListener("mouseenter", function(){
+    fixed.style.display = "block";
 });
+
+elemContainer.addEventListener("mouseleave", () => {
+    fixed.style.display = "none";
+})
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", () =>{
+        let image = e.getAttribute("data-image");
+        fixed.style.backgroundImage = `url(${image})`;
+    })
+})
+
