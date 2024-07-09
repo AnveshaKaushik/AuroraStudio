475d10bc96b70137581f184330d4792ec2d9d25c
Anvesha Kaushik <anvesha.tyagi261@gmail.com>
2024-07-09 21:03:48 +0530

    Made the website responsive

--- script.js
+++ script.js
@@ -51,7 +51,25 @@ function swiperAnim(){
     });
 }
 
+function menuAnimation() {
 
+    var menu = document.querySelector("nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("nav img")
+    var flag = 0
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else {
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
 
 swiperAnim()
 Anim()
+menuAnimation()
